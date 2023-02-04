74f35126d01fd779031f549c942c5918e791240f
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2023-02-04 15:40:12 +0200

    Basics of bot

--- lib/bender.ex
+++ lib/bender.ex
@@ -17,14 +17,6 @@ defmodule Bender do
     register_url = opts[:register_url] || "http://localhost:8080/register"
     bot_url = opts[:bot_url] || "http://localhost:4000/"
 
-    result = :httpc.request(:post, # Method
-      {register_url, # URL
-       [], # Headers
-       String.to_charlist("application/json"), # Content type to POST (must be an erlang list, not a binary)
-       Jason.encode!(%{playerName: name, url: bot_url})}, # Body contents
-      [], [sync: true]) # Http and other options
-
-    IO.puts(inspect(result))
-
+    Bender.GameState.register(name, register_url, bot_url)
   end
 end
--- lib/bender/game_state.ex
+++ lib/bender/game_state.ex
@@ -2,7 +2,10 @@ defmodule Bender.GameState do
   @moduledoc """
   Utilities for manipulating the game state.
   The server sends game state as JSON document.
+
   """
+
+ alias Bender.Mapper, as: M
 
   def from_json(json) do
     Jason.decode!(json)
@@ -17,7 +20,50 @@ defmodule Bender.GameState do
     from_json(str)
   end
 
-  def play(gs) do
-    "RIGHT"
+  def register(name, register_url, bot_url) do
+    {:ok, {_status, _headers, body}} = :httpc.request(:post, # Method
+      {register_url, # URL
+       [], # Headers
+       String.to_charlist("application/json"), # Content type to POST (must be an erlang list, not a binary)
+       Jason.encode!(%{playerName: name, url: bot_url})}, # Body contents
+      [], [sync: true]) # Http and other options
+
+    gs = from_json(body)
+
+    # Update map
+    M.map(get_in(gs, ["gameState", "map"]))
+  end
+
+  # Get position of thing as {x,y} tuple
+  defp pos(%{"position" => %{"x" => x, "y" => y}}), do: {x,y}
+
+  # Get direction from adjacent position
+  defp dir({x1,y1},{x2,y2}) do
+    cond do
+      x1 < x2 -> "RIGHT"
+      x2 < x1 -> "LEFT"
+      y1 < y2 -> "DOWN"
+      y2 < y1 -> "UP"
+    end
+  end
+
+  def play(%{"gameState" => %{"items" => items}=gs, "playerId" => id, "playerState" => me}) do
+    IO.puts("items: #{inspect(gs)}")
+    item = hd(items) # Take first item
+    item_pos = pos(item)
+    my_pos = pos(me)
+
+    ## Starting point, stupidly go to 1st item and try to get it.
+    ## should take into account what is the best item to get you can afford
+    ## if no items you can afford, go to exit
+
+    cond do
+      item_pos == my_pos ->
+        "PICK"
+      true ->
+        # Not at position, find path
+        {:path, [^my_pos, next_pos | _ ]} = M.path(my_pos, item_pos)
+        dir(my_pos, next_pos)
+    end
   end
 end
